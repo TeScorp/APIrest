24dc8195918a7ee543a70427861c68f35fac517a
Terra SARR <samba.co93@gmail.com>
2023-08-10 02:58:22 +0000

    API REst CRUD testing

---
+++ REst API/models/User.js
@@ -0,0 +1,23 @@
+//Définition d'un model schema
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
